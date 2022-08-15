9b53c841c40f2188e3ef9ad16b562e07e4b4bdcf
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-15 18:14:10 +0530

    Messed up the layout, found issue with background Image

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -15,9 +15,9 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset(
             "assets/login.png",
@@ -26,56 +26,62 @@ class _LoginPageState extends State<LoginPage> {
             scale: 0.8,
           ),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: _width * 0.1),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  "Welcome to",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: _width * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Welcome to",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
-                ),
-                Text(
-                  "ConnecTen",
-                  style: TextStyle(
-                    fontSize: 38,
-                    fontWeight: FontWeight.w800,
+                  const Text(
+                    "ConnecTen",
+                    style: TextStyle(
+                      fontSize: 38,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
-                Text(
-                  "\nBest cloud storage platform for all \nbusiness and individuals to \nmanage there data.",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
+                  const Text(
+                    "\nBest cloud storage platform for all \nbusiness and individuals to \nmanage there data.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-                Text(
-                  "\nJoin For Free.",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
+                  const Text(
+                    "\nJoin For Free.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: _width * 0.11,
-              vertical: _width * 0.05,
-            ),
-            child: Material(
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                alignment: Alignment.center,
-                height: _width * 0.1,
-                decoration: const BoxDecoration(
-                  color: Color(0xff567DF4),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Image.asset("assets/google.png"),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      // horizontal: _width * 0.11,
+                      vertical: _width * 0.05,
+                    ),
+                    child: Material(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        alignment: Alignment.center,
+                        height: _width * 0.1,
+                        decoration: const BoxDecoration(
+                          color: Color(0xff567DF4),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Image.asset("assets/google.png"),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: _height * 0.15,
+                  )
+                ],
               ),
             ),
           ),
