06e1019f38e62db336c1e8c899ef4546782ffb9f
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-15 23:08:19 +0530

    Initialized app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:connecten/view/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  // Initialize the application
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
