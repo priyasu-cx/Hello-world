6c4dcd39e223a5da0f0bd078aa01fa87d6ef6cf4
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-15 17:59:28 +0530

    Added the rest button. Still looks crappy

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -16,7 +16,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset(
@@ -29,7 +29,7 @@ class _LoginPageState extends State<LoginPage> {
             padding: EdgeInsets.symmetric(horizontal: _width * 0.1),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+              children: const [
                 Text(
                   "Welcome to",
                   style: TextStyle(
@@ -44,7 +44,39 @@ class _LoginPageState extends State<LoginPage> {
                     fontWeight: FontWeight.w800,
                   ),
                 ),
+                Text(
+                  "\nBest cloud storage platform for all \nbusiness and individuals to \nmanage there data.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "\nJoin For Free.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: _width * 0.11,
+              vertical: _width * 0.05,
+            ),
+            child: Material(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                alignment: Alignment.center,
+                height: _width * 0.1,
+                decoration: const BoxDecoration(
+                  color: Color(0xff567DF4),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Image.asset("assets/google.png"),
+              ),
             ),
           ),
         ],
