da18ae713570b94fccc98444311ff910ecb79b22
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-17 11:08:05 +0530

    Created form Screen

---
+++ lib/view/form_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
