4a8de7a5556725c805cf5466f849593c228a7ae0
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-17 11:26:04 +0530

    Added from data

--- lib/view/form_screen.dart
+++ lib/view/form_screen.dart
@@ -8,8 +8,52 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+
+  // Form key
+  final _formKey = GlobalKey<FormState>();
+
+  // Editing controller
+  final TextEditingController nameController = new TextEditingController();
+  final TextEditingController designationController = new TextEditingController();
+  final TextEditingController bioController = new TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
+
+    // Name field
+    final nameField  = TextFormField(
+      autofocus: false,
+      controller: nameController,
+      keyboardType: TextInputType.name,
+      onSaved: (value) {
+        nameController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+    );
+
+    // Designation Field
+    final designationField  = TextFormField(
+      autofocus: false,
+      controller: designationController,
+      keyboardType: TextInputType.text,
+      onSaved: (value) {
+        designationController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+    );
+
+    // Bio Field
+    final bioField  = TextFormField(
+      autofocus: false,
+      controller: bioController,
+      keyboardType: TextInputType.text,
+      onSaved: (value) {
+        bioController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+    );
+
     return Scaffold();
   }
 }
