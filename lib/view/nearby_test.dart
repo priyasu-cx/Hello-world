99f6dd67549857956477f4411059146d0edf878d
priyasu-cx <codexposer.priyasu25@gmail.com>
2022-08-19 03:02:43 +0530

    Design Changes

--- lib/view/nearby_test.dart
+++ lib/view/nearby_test.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 import 'dart:typed_data';
-
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
