e9d5346de4a4fed5516d7127a1be692e915bc70f
shubhayu-64 <majumdarshubhayu@gmail.com>
2022-08-20 16:37:27 +0530

    Fixed some business logic

--- lib/view/profile_dialog.dart
+++ lib/view/profile_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:connecten/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // void ProfileDialog(BuildContext context) {
@@ -23,65 +24,69 @@ import 'package:url_launcher/url_launcher.dart';
 
 Future ProfileDialog(allUserData, context) => showDialog(
     context: context,
-    builder: (context) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        //title: Text(uid+" Link"),
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: Get.width * 0.1,
-                backgroundColor: primarybgcolor,
-                backgroundImage: AssetImage("assets/Avatar.png"),
-                //foregroundImage: sp.imageUrl == null ? AssetImage("assets/Avatar.png") : NetworkImage(sp.imageUrl),
-                //foregroundImage: NetworkImage(sp.imageUrl!),
-              ),
-              SizedBox(
-                height: Get.height * 0.01,
-              ),
-              Text(
-                "Social Links",
-                style: TextStyle(
-                  letterSpacing: 1,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-                textAlign: TextAlign.left,
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    social_link(context, 1, "assets/linkedin.png", "Linkedin",
-                        allUserData["linkedIn"]),
-                    social_link(context, 2, "assets/github.png", "Github",
-                        allUserData["github"]),
-                    social_link(context, 3, "assets/website.png", "Portfolio",
-                        allUserData["portfolio"]),
-                    social_link(context, 4, "assets/twitter.png", "Twitter",
-                        allUserData["twitter"]),
-                    // Row(
-                    //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    //     children: [
-                    //       social_link(context, 1, "assets/linkedin.png", "Linkedin", "link"),
-                    //       social_link(context, 2, "assets/github.png", "Github", "link"),
-                    //     ]),
-                    // Row(
-                    //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    //     children: [
-                    //       social_link(context,3, "assets/website.png", "Portfolio", "link"),
-                    //       social_link(context,4, "assets/twitter.png", "Twitter", "link"),
-                    //     ]),
-                  ],
-                ),
-              )
-            ],
+    builder: (context) {
+      final datacount = GetStorage();
+      return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
           ),
-        )));
+          //title: Text(uid+" Link"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: Get.width * 0.1,
+                  backgroundColor: primarybgcolor,
+                  backgroundImage: AssetImage("assets/Avatar.png"),
+                  foregroundImage: NetworkImage(allUserData["imageUrl"]),
+                  //foregroundImage: sp.imageUrl == null ? AssetImage("assets/Avatar.png") : NetworkImage(sp.imageUrl),
+                  //foregroundImage: NetworkImage(sp.imageUrl!),
+                ),
+                SizedBox(
+                  height: Get.height * 0.01,
+                ),
+                Text(
+                  "Social Links",
+                  style: TextStyle(
+                    letterSpacing: 1,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      social_link(context, 1, "assets/linkedin.png", "Linkedin",
+                          allUserData["linkedIn"]),
+                      social_link(context, 2, "assets/github.png", "Github",
+                          allUserData["github"]),
+                      social_link(context, 3, "assets/website.png", "Portfolio",
+                          allUserData["portfolio"]),
+                      social_link(context, 4, "assets/twitter.png", "Twitter",
+                          allUserData["twitter"]),
+                      // Row(
+                      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      //     children: [
+                      //       social_link(context, 1, "assets/linkedin.png", "Linkedin", "link"),
+                      //       social_link(context, 2, "assets/github.png", "Github", "link"),
+                      //     ]),
+                      // Row(
+                      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      //     children: [
+                      //       social_link(context,3, "assets/website.png", "Portfolio", "link"),
+                      //       social_link(context,4, "assets/twitter.png", "Twitter", "link"),
+                      //     ]),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ));
+    });
 Widget social_link(context, index, image, text, link) {
   return Container(
     width: Get.width * 0.3,
